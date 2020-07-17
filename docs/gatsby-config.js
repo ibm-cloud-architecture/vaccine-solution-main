f34560efb868b9063c3ae3c7b726b73951f9f287
Jerome Boyer <boyerje@us.ibm.com>
2020-07-17 09:18:41 -0700

    change in index and first page pics

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: 'IBM Garage: A Cloud Pak Show Case',
-    description: 'This project represents an integrated solution to deliver Vaccine WW at billions of lot.',
+    description: 'This project represents an integrated solution to deliver Vaccine WW at billions of lots.',
     keywords: 'gatsby,theme,carbon',
     author: 'IBM Garage Solution Engineering'
   },
-  pathPrefix: '/vaccine-cold-chain/vaccine-cold-chain-main/',
+  pathPrefix: '/vaccine-solution-main',
   plugins: [
     {
       resolve: 'gatsby-theme-carbon',
@@ -13,7 +13,7 @@ module.exports = {
         isSearchEnabled: true,
         titleType: 'append',
         repository: {
-          baseUrl: 'https://github.ibm.com/vaccine-cold-chain/vaccine-cold-chain-main',
+          baseUrl: 'https://github.com/ibm-cloud-architecture/vaccine-solution-main',
           subDirectory: '/docs',
           branch: 'master'
         }
