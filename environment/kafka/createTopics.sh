a12d388aaeb3e74938ebb6ea9d6564727c33407f
Jerome Boyer <boyerje@us.ibm.com>
2020-11-19 19:13:43 -0800

    Work on orderms note

---
+++ environment/kafka/createTopics.sh
@@ -0,0 +1,7 @@
+oc exec -ti  light-es-kafka-2  -c kafka  bash <<EOF
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic db_history_vaccine_orders
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic vaccine_shipments
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic transportation
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic reefer_telemetries
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic vaccine_lots
+EOF
