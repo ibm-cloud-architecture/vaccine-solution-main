e786f5aa011b0415ad99ed8ec97f64376ec54533
Jerome Boyer <boyerje@us.ibm.com>
2020-12-10 18:50:52 -0800

    update to order and add journey

--- environment/kafka/createTopics.sh
+++ environment/kafka/createTopics.sh
@@ -1,4 +1,4 @@
-oc exec -ti  light-es-kafka-2  -c kafka  bash <<EOF
+docker exec -ti  kafka   bash <<EOF
 /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic db_history_vaccine_orders
 /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic vaccine_shipments
 /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic transportation
