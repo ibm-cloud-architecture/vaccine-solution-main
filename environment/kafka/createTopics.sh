a199edd3c6b64828694db97b7313f826b75b75c9
Jerome Boyer <boyerje@us.ibm.com>
2021-04-12 08:02:43 -0700

    update to order diagram

--- environment/kafka/createTopics.sh
+++
@@ -1,7 +0,0 @@
-docker exec -ti  kafka   bash <<EOF
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic db_history_vaccine_orders
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic vaccine_shipments
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic transportation
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic reefer_telemetries
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create  --replication-factor 1 --partitions 1 --topic vaccine_lots
-EOF
--- environment/kafka/defineTopics.sh
+++
@@ -1,5 +0,0 @@
-cloudctl es topic-create --name vaccine_orders --partitions 1 --replication-factor 3
-cloudctl es topic-create --name reefer_telemetries --partitions 1 --replication-factor 3
-cloudctl es topic-create --name vaccine_shipments --partitions 1 --replication-factor 3
-cloudctl es topic-create --name vaccine_lots --partitions 1 --replication-factor 3
-cloudctl es topic-create --name transportation --partitions 1 --replication-factor 3
